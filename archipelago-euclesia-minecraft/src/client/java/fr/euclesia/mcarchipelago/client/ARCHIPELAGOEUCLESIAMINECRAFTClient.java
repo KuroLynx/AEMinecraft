4dfe2a33917ad473f8cb4495673ee3f0a1163a53
riege <victor.riegert@epitech.eu>
2026-05-14 21:45:47 +0200

    feat: add mod Template to project

---
+++ archipelago-euclesia-minecraft/src/client/java/fr/euclesia/mcarchipelago/client/ARCHIPELAGOEUCLESIAMINECRAFTClient.java
@@ -0,0 +1,10 @@
+package fr.euclesia.mcarchipelago.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ARCHIPELAGOEUCLESIAMINECRAFTClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
