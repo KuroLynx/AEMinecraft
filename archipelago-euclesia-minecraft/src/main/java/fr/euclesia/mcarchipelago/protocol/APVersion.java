adb976c829dba51afdf2c6691eb7b849d9111680
KuroLynx <benjamlelynx@gmail.com>
2026-05-29 17:56:08 +0200

    fix: Corrected version jsonification

--- archipelago-euclesia-minecraft/src/main/java/fr/euclesia/mcarchipelago/protocol/APVersion.java
+++ archipelago-euclesia-minecraft/src/main/java/fr/euclesia/mcarchipelago/protocol/APVersion.java
@@ -1,6 +1,7 @@
 package fr.euclesia.mcarchipelago.protocol;
 
 import com.google.gson.JsonObject;
+import org.jspecify.annotations.NonNull;
 
 public record APVersion(int major, int minor, int build) {
     public static final APVersion V0_6_0 = new APVersion(0, 6, 0);
@@ -10,6 +11,7 @@ public record APVersion(int major, int minor, int build) {
         json.addProperty("major", major);
         json.addProperty("minor", minor);
         json.addProperty("build", build);
+        json.addProperty("class", "Version");
         return json;
     }
 }
